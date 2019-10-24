7c6c75fb3147f33c44b5605567893bdbff57f2d0
J. Manrique Lopez de la Fuente <jsmanrique@bitergia.com>
2019-10-24 22:02:10 +0200

    Fix a typo in inline list of actions for provisioner remote-exec
    Signed-off-by: J. Manrique Lopez de la Fuente <jsmanrique@bitergia.com>

--- aws/main.tf
+++ aws/main.tf
@@ -52,7 +52,7 @@ resource "aws_lightsail_instance" "grimoirelab_demo" {
       "sudo apt-get update",
       "sudo apt-get -y install docker-ce docker-ce-cli containerd.io",
       "sudo groupadd docker",
-      "sudo usermod -aG docker $USER"
+      "sudo usermod -aG docker $USER",
       "sudo curl -L \"https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose"
     ]
